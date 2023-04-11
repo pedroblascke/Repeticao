54e52ac207f7fa755d5b756d68a9aaf87d6e1d57
Pedro Blascke <pedro-lucasm@hotmail.com>
2023-04-11 20:52:10 -0300

    estruturaRepeticao

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int i;
+        i = 1;
+        while(i <= 10) {
+            System.out.print(i +" ");
+            i = i + 1;
+        }
+    }
+}
