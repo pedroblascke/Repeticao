bd597c11a2756e001e6da907b0b8554bff062f6f
Pedro Blascke <pedro-lucasm@hotmail.com>
2023-04-11 21:30:21 -0300

    For

--- src/App.java
+++ src/App.java
@@ -2,10 +2,12 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int i;
-        i = 0;
-        do {
+        
+
+        for(i = 0; (i < 10); i = i + 1) {
+        
             System.out.print(i +" ");
-            i = i + 1; 
-        } while (i < 10);
+             
+        } 
     }
 }
