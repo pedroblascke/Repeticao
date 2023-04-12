60923071995279984def51163f453bc7769241ed
Pedro Blascke <pedro-lucasm@hotmail.com>
2023-04-11 21:27:53 -0300

    doWhile

--- src/App.java
+++ src/App.java
@@ -2,10 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int i;
-        i = 1;
-        while(i <= 10) {
+        i = 0;
+        do {
             System.out.print(i +" ");
-            i = i + 1;
-        }
+            i = i + 1; 
+        } while (i < 10);
     }
 }
